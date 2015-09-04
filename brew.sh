1c325fd685b2db00d433a17156133ba9b2545826
Your Name <username@domain.com>
2015-09-04 09:55:49 +0200

    updating dotfiles

--- brew.sh
+++ brew.sh
@@ -55,6 +55,7 @@ brew install heroku-toolbelt
 brew install rbenv
 brew install ruby-build
 brew install postgresql
+brew install mongodb
 
 # Remove outdated versions from the cellar.
 brew cleanup
